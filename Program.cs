4f23d1153d0055a2ba0f24e20fa0b63493652d19
defaultxx00 <default00x@gmail.com>
2026-05-18 08:54:18 -0300

    fix layout

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddScoped<QueryMonitorInterceptor>();
 
 builder.Services.AddDbContext<AppDbContext>((sp, options) =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSecundaria"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao"));
     options.AddInterceptors(sp.GetRequiredService<QueryMonitorInterceptor>());
 });
 
---
+++ det.md
@@ -0,0 +1,7 @@
+EXPLICANDO AONDE DEVEM ROLAR AS MUDANÇAS
+
+    o campo de "comantario adicionado" esta mal organizado quando aparece, tente deixar mais alinhado com o design todo da pagina;
+
+    a area de "meus posts" nao esta estilizada, concerte por favor;
+
+    
