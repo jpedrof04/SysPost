33a417130305c68b8c27ce4fc89ba4722da99fe8
defaultxx00 <default00x@gmail.com>
2026-05-18 12:31:56 -0300

    details

--- obj/Debug/net9.0/SysPost.AssemblyInfo.cs
+++ obj/Debug/net9.0/SysPost.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysPost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad62681d259fd08385ed3c68311f50e72a52381")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee4bf59d345b003ccdfee3e73a0c3e3a973adca")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysPost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysPost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
